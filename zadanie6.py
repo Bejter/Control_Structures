009d1551209922d4f12410336b2a44285a46f010
Bejter <kacper.majda62@gmail.com>
2024-11-17 19:08:03 +0100

    zadanie 5 oraz troche zadania 6

---
+++ zadanie6.py
@@ -0,0 +1,16 @@
+#2
+text = "This is a sample text."
+vowels = "aeiouAEIOU"
+vowel_count = 0
+
+i = 0
+
+while i < len(text):
+    if text[i] in vowels:
+        vowel_count += 1
+    i+=1
+
+print(f"The number of vowels in the text is: {vowel_count}")
+
+#3
+
